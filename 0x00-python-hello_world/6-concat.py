96ede20790129eb3cf65ce976cad31e3b456b03c
ted kouhouenou <teddams047@gmail.com>
2022-11-28 16:33:51 +0100

    6 check

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
-str2 = "Schoo"
-str2 += 'l'
-print("Welcome to " + str1 + " " + str2 + "!")
+str2 = "School"
+str1 += (" " + str2)
+print("Welcome to " + str1 + "!")
