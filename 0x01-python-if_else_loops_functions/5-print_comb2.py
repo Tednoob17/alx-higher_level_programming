5bd52bc7978f9a0a37f6022f4919eba8bfa70dd7
ted kouhouenou <teddams047@gmail.com>
2022-11-29 17:11:53 +0100

    check 655

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for a in range(0, 100):
-	if a == 99:
-    		print("{}".format(a))
-	else:
-    		print(f"{a:0>2}",end=", ")
+    if a == 99:
+        print("{}".format(a))
+    else:
+        print(f"{a:0>2}", end=", ")
