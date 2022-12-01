7f36b7e79950982985f7843834df065af11948a0
= <=>
2022-12-01 14:35:41 +0100

    2 check

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    n = len(sys.argv) - 1
+    if argv == 0:
+        print("0 argument")
+    elif argv == 1:
+        print("1 argument")
+    else:
+        print("{} arguments:".format(n))
 
+        for i in rang(n):
+            print("{}: {}".format(i + 1, argv[i + 1]))
