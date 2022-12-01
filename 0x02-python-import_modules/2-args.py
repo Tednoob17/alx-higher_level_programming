4023670ba913ca205a69c96a76be7d032cc6ece7
= <=>
2022-12-01 14:58:39 +0100

    grunch 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,9 @@ if __name__ == "__main__":
 
     n = len(sys.argv) - 1
     if n == 0:
-        print("0 arguments")
+        print("0 arguments.")
     elif n == 1:
-        print("1 argument")
+        print("1 argument:")
     else:
         print("{} arguments:".format(n))
 
--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -2,11 +2,14 @@
 def add(a, b):
     return (a + b)
 
+
 def sub(a, b):
     return (a - b)
 
+
 def mul(a, b):
     return (a * b)
+
 
 def div(a, b):
     return int(a / b)
