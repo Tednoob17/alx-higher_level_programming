cae6de1559637f70b72cc042cbe8a23174c44147
= <=>
2022-12-01 15:43:53 +0100

    chunk 5

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     number = 0
-    for i in range (len(sys.argv) - 1):
+    for i in range(len(sys.argv) - 1):
         number += int(sys.argv[i + 1])
     print("{}".format(number))
