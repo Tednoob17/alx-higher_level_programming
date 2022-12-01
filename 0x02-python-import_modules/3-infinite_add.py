8fe1e52f441ed54923f5429377c3df9fcf099b68
= <=>
2022-12-01 15:46:44 +0100

    chunk 9

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print the addition of all arguments."""
     import sys
-    number = 0
+
+    total = 0
     for i in range(len(sys.argv) - 1):
-        number += int(sys.argv[i + 1])
-    print("{}".format(number))
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
