e6bec8b1f3d7fffde44102e78023133941aa00b1
= <=>
2022-12-01 15:13:37 +0100

    junkie 44

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     import sys
 
-    n = len(sys.argv) - 1
-    if n == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif n == 1:
+    elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(n))
-    for i in range(n):
+        print("{} arguments:".format(count))
+    for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
