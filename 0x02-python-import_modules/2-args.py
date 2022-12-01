e5067632d5f352c32c25aa908080f3fccb00dc7d
= <=>
2022-12-01 14:53:21 +0100

    grunch 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     n = len(sys.argv) - 1
     if n == 0:
-        print("0 argument")
+        print("0 arguments")
     elif n == 1:
         print("1 argument")
     else:
