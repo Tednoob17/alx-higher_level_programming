e73faf8f466dde85f505a39d230e064fe949ba02
Ted KOUHOUENOU <teddams047@gmail.com>
2022-12-03 12:42:16 +0100

    push

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
+
     """
     Print a list of integers
     ...
@@ -12,5 +12,7 @@ def print_list_integer(my_list=[]):
     Return:
         None
     """
+
+def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
         print("{}".format(my_list[i]))
