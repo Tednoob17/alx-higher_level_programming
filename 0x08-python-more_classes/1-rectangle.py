192789e3572b5ca405af99661ec140472bc1ea34
ted kouhouenou <teddams047@gmail.com>
2023-01-03 04:35:25 +0100

    middle file

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a Rectangle class."""
+
+
+class Rectangle(object):
+    """Represent a Rectangle."""
+
+    def __init__(self, value):
+        self.value = value
+    def width(self):
+        if (type(self.value) != int)
+                
