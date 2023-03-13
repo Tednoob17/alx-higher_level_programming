76184b6ab02443d6b05d139513e943323fd9a492
ted kouhouenou <teddams047@gmail.com>
2023-03-13 19:43:43 +0100

    sup finish

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,3 @@
 #!/usr/bin/node
-const arg = process.argv.length;
-/*
-if (arg === 2) {
-	console.log("No argument")
-}else if (arg === 3) {
-	console.log("Argument found")
-} else {
-	console.log("Arguments found")
-}
-*/
-console.log(arg === 2 ? 'No argument' : arg === 3 ?  'Argument found' : 'Arguments found');
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
